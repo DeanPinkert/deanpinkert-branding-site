9d79ea0c403527aa5b3f5370aaec68af2d608f64
CC <caledoniaconsult@gmail.com>
2024-07-05 13:21:23 -0400

    wip - config edit for prepend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/deanpinkert-branding-site/',
+    base: '/',
     plugins: [
       vue(),
       ViteImageOptimizer({
