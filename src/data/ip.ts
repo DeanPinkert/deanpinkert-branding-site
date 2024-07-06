01776c8b27b05544bcdb2b52b9e5f1884bd51328
CC <caledoniaconsult@gmail.com>
2024-07-06 13:32:10 -0400

    wip - edit asset locations

--- src/data/ip.ts
+++ src/data/ip.ts
@@ -3,7 +3,7 @@ export default [
         id: 0,
         title: 'Take Down Or Stay Down: Digital Piracy And Trade Negotiation',
         src: 'https://s3.amazonaws.com/hhr-web/files/Take-Down-Or-Stay-Down-Digital-Piracy-And-Trade-Negotiation.pdf',
-        backgroundImg: 'https://caledoniaadmin.github.io/dpc/luther-m-e-bottrill-EsBufnuK4NE-unsplash.jpg',
+        backgroundImg: 'https://deanpinkert.com/luther-m-e-bottrill-EsBufnuK4NE-unsplash.jpg',
         mediaType: '',
         payWall: '',
         source: 'HHR'
@@ -12,7 +12,7 @@ export default [
         id: 1,
         title: 'One ITC chief found Samsung to fail to offer Apple FRAND licensing terms to its UMTS patents',
         src: 'http://www.fosspatents.com/2013/07/one-itc-chief-found-samsung-to-fail-to.html',
-        backgroundImg: 'https://caledoniaadmin.github.io/dpc/zana-latif-rSp7jTx_FvI-unsplash.jpg',
+        backgroundImg: 'https://deanpinkert.com/zana-latif-rSp7jTx_FvI-unsplash.jpg',
         mediaType: '',
         payWall: '',
         source: 'FossPants'
@@ -21,7 +21,7 @@ export default [
         id: 2,
         title: 'US Complaints About Technology Transfer In China: Negotiating The Endgame',
         src: 'https://www.ip-watch.org/2019/01/24/us-complaints-technology-transfer-china-negotiating-endgame/',
-        backgroundImg: 'https://caledoniaadmin.github.io/dpc/nevergg-DhOPjNWBbZE-unsplash.jpg',
+        backgroundImg: 'https://deanpinkert.com/nevergg-DhOPjNWBbZE-unsplash.jpg',
         mediaType: '',
         payWall: '',
         source: 'IP-Watch'
@@ -30,7 +30,7 @@ export default [
         id: 3,
         title: 'U.S. response to Chinese IP theft could skirt WTO rules',
         src: 'https://www.axios.com/2018/03/19/us-response-to-chinese-ip-theft-could-skirt-wto-rules',
-        backgroundImg: 'https://caledoniaadmin.github.io/dpc/alexandru-bogdan-ghita-eM6WUs4nKMY-unsplash.jpg',
+        backgroundImg: 'https://deanpinkert.com/alexandru-bogdan-ghita-eM6WUs4nKMY-unsplash.jpg',
         mediaType: '',
         payWall: '',
         source: 'Axios'
